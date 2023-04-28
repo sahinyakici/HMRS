38325cf90e79f76fca016385ce6158f7c3ce3573
Şahin Yakıcı <shnykc@hotmail.com>
2023-04-28 23:10:10 +0300

    Initial commit

---
+++ src/test/java/kodlamaio/HMRS/HmrsApplicationTests.java
@@ -0,0 +1,13 @@
+package kodlamaio.HMRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
